30e8f4bed1559e7b981079bc4cd4fa76c47c6e4d
William01br <williamstcorreia123@gmail.com>
2026-03-14 21:05:53 -0300

    refactor: implements gateway logic in purchase endpoint

--- app/controllers/purchases_controller.ts
+++ app/controllers/purchases_controller.ts
@@ -1,12 +1,15 @@
 import { PurchaseService } from '#services/purchase_service'
 import { purchaseValidator } from '#validators/purchase'
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
+@inject()
 export default class PurchasesController {
+  constructor(private readonly purchaseService: PurchaseService) {}
   async handle({ request, response }: HttpContext) {
     const payload = await request.validateUsing(purchaseValidator)
 
-    const result = await PurchaseService.handle(payload)
+    const result = await this.purchaseService.handle(payload)
 
     response.ok({ data: result })
   }
